0f54a0b0a7017eef0de77cdf11cc242920d46277
justEstif <95101485+justEstif@users.noreply.github.com>
2022-06-23 18:08:16 -0400

    initial commit with npm packages setup

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import {SBtn} from "./SApp"
+function App() {
+  return (
+    <div>Hello World <SBtn>Sign Up</SBtn></div>
+  );
+}
+
+export default App;
---
+++ src/SApp.js
@@ -0,0 +1,11 @@
+import tw from "tailwind-styled-components/dist/tailwind";
+
+const SBtn = tw.button`
+  py-2 
+  px-8 
+  rounded-lg
+  bg-blue-500 
+  text-white
+`
+
+export {SBtn}
