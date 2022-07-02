91fbdf34e075466629e0617f34feb3f135c11309
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-02 16:46:06 -0400

    added useAuthListener

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
+import useAuthListener from "./hooks/use-auth-listener";
 
 const Login = lazy(() => import("./pages/login"));
 const SignUp = lazy(() => import("./pages/sign-up"));
@@ -8,6 +9,7 @@ const Dashboard = lazy(() => import("./pages/dashboard"));
 const NotFound = lazy(() => import("./pages/not-found"));
 
 export default function App() {
+  const { user } = useAuthListener();
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
