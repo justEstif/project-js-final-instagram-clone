de656b5a4e65d53fb6c61d648868c4f66a30f484
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-03 17:49:50 -0400

    removed comments

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -3,7 +3,6 @@ import FirebaseContext from "../context/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 
 const useAuthListener = () => {
-  // go through the state, and set the user
   const [user, setUser] = useState(
     JSON.parse(localStorage.getItem("authUser"))
   );
@@ -12,19 +11,15 @@ const useAuthListener = () => {
     firebase: { auth },
   } = useContext(FirebaseContext);
   useEffect(() => {
-    // when there is an auth change
     const listener = onAuthStateChanged(auth, (authUser) => {
-      // if we have a user -> we can store the user in Local storagee
       if (authUser) {
         localStorage.setItem("authUser", JSON.stringify(authUser));
         setUser(authUser);
       } else {
-        // if there is a logout -> remove the user item
         localStorage.removeItem("authUser");
         setUser(null);
       }
     });
-    // clean
     return () => listener();
   }, [auth]);
   return { user };
