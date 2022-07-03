8ebc4f0524b5cbdf53e456f8bccf3c192d9594ad
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-03 16:31:22 -0400

    created getUserByUserId function

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,10 +1,21 @@
 import { firebase } from "../lib/firebase";
 import { collection, getDocs, query, where } from "firebase/firestore";
+const { db } = firebase;
 
 export async function doesUsernameExist(username) {
-  const { db } = firebase;
   const usersRef = collection(db, "users");
   const q = query(usersRef, where("username", "==", username.toLowerCase()));
   const querySnapshot = await getDocs(q);
   return querySnapshot.docs.length > 0; // return true if some doc exists
+}
+
+export async function getUserByUserId(userId) {
+  const usersRef = collection(db, "users");
+  const q = query(usersRef, where("userId", "==", userId));
+  const querySnapshot = await getDocs(q);
+  const user = querySnapshot.docs.map((item) => ({
+    ...item.data(),
+    docId: item.id,
+  }));
+  return user;
 }
