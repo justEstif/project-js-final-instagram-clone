2210a59ea5015853e10d81391ce8efec89023eab
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-04 12:59:20 -0400

    updates the suggested profiles in useEffect

--- src/components/sidebar/suggestions.js
+++ src/components/sidebar/suggestions.js
@@ -1,6 +1,14 @@
 // , we don't want to show users that are already in followers list
-const Suggestions = () => {
-  return <div>Suggestions</div>;
+  const [profiles, setProfiles] = useState(null);
+
+  useEffect(() => {
+    const suggestedProfiles = async () => {
+      const response = await getSuggestedProfiles(userId, following);
+      setProfiles(response);
+      console.log("profiles:", profiles);
+    };
+    userId && suggestedProfiles();
+  }, [userId]);
 };
 
 export default Suggestions;
