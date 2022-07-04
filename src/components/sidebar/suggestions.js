aaec47cde44bdf7d35a2a89650b7b92edc2f397b
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-04 13:00:10 -0400

    conditional rendering of suggestions

--- src/components/sidebar/suggestions.js
+++ src/components/sidebar/suggestions.js
@@ -9,6 +9,31 @@
     };
     userId && suggestedProfiles();
   }, [userId]);
+
+  return (
+    <Fragment>
+      {!profiles ? (
+        <Skeleton count={1} height={150} className="mt-5" />
+      ) : profiles.length > 0 ? (
+        <div className="rounded flex flex-col">
+          <div className="text-sm flex items-center justify-between mb-2">
+            <p className="font-bold text-gray-base">Suggestions for you</p>
+          </div>
+          <div className="mt-4 grid gap-5">
+            {profiles.map((profile) => (
+              <SuggestedProfile
+                key={profile.docId}
+                useDocId={profile.docId}
+                username={profile.username}
+                profileId={profile.userId}
+                userId={userId}
+              />
+            ))}
+          </div>
+        </div>
+      ) : null}
+    </Fragment>
+  );
 };
 
 export default Suggestions;
