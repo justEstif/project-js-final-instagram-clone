903858b1dfeae2642b7eca16df8f3b8a17f119d0
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-04 12:58:30 -0400

    passed props to User and Suggestions

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -3,14 +3,15 @@ import User from "./user";
 import Suggestions from "./suggestions";
 
 const Sidebar = () => {
+  // , get the info of the logged in user
   const {
-    user: { fullName, username, userId },
+    user: { fullName, username, userId, following },
   } = useUser();
 
   return (
     <div className="p-4">
-      <User />
-      <Suggestions />
+      <User username={username} fullName={fullName} />
+      <Suggestions userId={userId} following={following} />
     </div>
   );
 };
