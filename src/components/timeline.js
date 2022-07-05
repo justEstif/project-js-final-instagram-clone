ecdcee0ba5672f27529d77ee787d8d24af05b89f
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-05 14:09:57 -0400

    created timeline component with Skeleton

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,5 +1,22 @@
+import { Fragment } from "react";
+import Skeleton from "react-loading-skeleton";
+import usePhotos from "../hooks/use-photos";
+import Post from "./post";
 const Timeline = () => {
-  return <div className="container col-span-2">Timeline</div>;
+  const { photos } = usePhotos();
+  return (
+    <div className="container col-span-2">
+      {!photos ? (
+        <Fragment>
+          <Skeleton count={4} width={640} height={500} className="mb-5" />
+        </Fragment>
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <Post key={content.docId} content={content} />)
+      ) : (
+        <p className="text-center text-2xl">Follow people to see photos</p>
+      )}
+    </div>
+  );
 };
 
 export default Timeline;
