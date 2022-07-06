5d8740d8fbfbaddc7678c10821a1146ca3243b79
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-06 11:11:33 -0400

    made routes pretected

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import UserContext from "./context/user";
 import useAuthListener from "./hooks/use-auth-listener";
+import ProtectedRoute from "./helpers/protected.route";
+import IsUserLoggedIn from "./helpers/is-user-logged-in";
 
 const Login = lazy(() => import("./pages/login"));
+const Profile = lazy(() => import("./pages/profile"));
 const SignUp = lazy(() => import("./pages/sign-up"));
 const Dashboard = lazy(() => import("./pages/dashboard"));
 const NotFound = lazy(() => import("./pages/not-found"));
@@ -16,9 +19,42 @@ export default function App() {
       <Router>
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
-            <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
-            <Route path={ROUTES.LOGIN} element={<Login />} />
-            <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+            <Route
+              path={ROUTES.DASHBOARD}
+              element={
+                <ProtectedRoute user={user}>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+              exact
+            />
+
+            <Route
+              path={ROUTES.LOGIN}
+              element={
+                <IsUserLoggedIn user={user}>
+                  <Login />
+                </IsUserLoggedIn>
+              }
+            />
+
+            <Route
+              path={ROUTES.SIGN_UP}
+              element={
+                <IsUserLoggedIn user={user}>
+                  <SignUp />
+                </IsUserLoggedIn>
+              }
+            />
+            <Route
+              path={ROUTES.PROFILE}
+              element={
+                <IsUserLoggedIn user={user}>
+                  <Profile />
+                </IsUserLoggedIn>
+              }
+            />
+
             <Route path={ROUTES.NOT_FOUND} element={<NotFound />} />
           </Routes>
         </Suspense>
