3c6e5537e23f63952114bf093df7dbe835791356
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-07 10:56:34 -0400

    fixed?

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Fragment, lazy, Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import * as ROUTES from "./constants/routes";
 import UserContext from "./context/user";
