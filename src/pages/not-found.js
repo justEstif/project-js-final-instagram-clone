5b58390cab9e6e5380359e453ced2ecb54df0465
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-07 08:58:13 -0400

    added header to not found page

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
+import Header from "../components/header";
 const NotFound = () => {
   useEffect(() => {
     document.title = "Page Not Found";
   }, []);
   return (
     <div className="bg-gray-background">
+      <Header />
       <div className="mx-auto max-w-screen-lg">
         <p className="text-center text-2xl">Not Found!</p>
       </div>
